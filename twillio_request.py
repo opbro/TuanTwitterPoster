06f36d68b09e43f707e265e5421ac971fbba2a67
root <root@keelserver.localdomain>
2017-02-12 18:04:27 +0000

    You can now text number with #t

---
+++ twillio_request.py
@@ -0,0 +1,35 @@
+from datetime import date
+from twilio.rest import TwilioRestClient
+import twitter_request
+import json
+
+# To find these visit https://www.twilio.com/user/account
+
+TUAN_ID= '#T'
+def main():
+	Key_file_path="/root/venv0/key.json"
+
+	with open(Key_file_path) as json_data:
+		data = json.load(json_data)
+	json_data.close()
+	ACCOUNT_SID = data["ACCOUNT_SID"]
+	AUTH_TOKEN = data["AUTH_TOKEN"]
+	PHONE_NUMBER = data["PHONE_NUMBER"]
+	TUAN_ID= '#T'
+
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+	messages = client.messages.list()
+	Message_to_post_twitter = []
+
+	for message in messages:
+	    if message.body[:2].upper() == TUAN_ID:
+	    	Message_to_post_twitter.append(message.body[2:].lstrip())
+	    message.delete()
+
+	for each in Message_to_post_twitter:
+		twitter_request.POST_TO_TWITTER(each)
+		print "{0} posted.".format(each)
+
+
+if __name__ == "__main__":
+	main()
--- twitter_request.py
+++ twitter_request.py
@@ -5,24 +5,21 @@ from twython import Twython
 import time
 import argparse
 
-parser = argparse.ArgumentParser(description='Input Appliance.config file.')
-parser.add_argument('-m',action='store',dest='message')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Input Appliance.config file.')
+# parser.add_argument('-m',action='store',dest='message')
+# args = parser.parse_args()
 
-message = args.message
-print message
-Key_file_path="/root/venv0/key.json"
+# message = args.message
+def POST_TO_TWITTER(message):
+	Key_file_path="/root/venv0/key.json"
 
-with open(Key_file_path) as json_data:
-	data = json.load(json_data)
-json_data.close()
+	with open(Key_file_path) as json_data:
+		data = json.load(json_data)
+	json_data.close()
+	APP_KEY = data['API_KEY']
+	APP_SECRET = data['SUPER_SECRET_KEY']
+	OAUTH_TOKEN = data['OAUTH_TOKEN']
+	OAUTH_TOKEN_SECRET = data['OAUTH_TOKEN_SECRET']
 
-
-
-APP_KEY = data['API_KEY']
-APP_SECRET = data['SUPER_SECRET_KEY']
-OAUTH_TOKEN = data['OAUTH_TOKEN']
-OAUTH_TOKEN_SECRET = data['OAUTH_TOKEN_SECRET']
-
-twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-twitter.update_status(status=message)
+	twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+	twitter.update_status(status=message)
