975c73c971f52f76367d690c2ea4863ccb2aade4
root <root@keelserver.localdomain>
2017-02-12 01:52:05 +0000

    added ability to post to twitter

--- twitter_request.py
+++ twitter_request.py
@@ -1,3 +1,9 @@
-import time
+import json
+from twython import Twython
 
-print "yerp"
+
+Key_file_path="/root/venv0/key.json"
+
+twitter = Twython(APP_KEY, APP_SECRET
+                  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitter.update_status(status='See how easy using Twython is!')
